5ca0b5e883bede31b9d1425c55100634755857b2
Gistya Eusebio <gistya@gmail.com>
2013-06-17 15:04:59 -0700

    It's alive!

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,8 @@ var fs   = require('fs'),
 
 exports.index = function(req, res){
     //var doc = fs.readFileSync('./files/Questions.yaml', 'utf8');
-    //console.log(doc);
-    var JString = jsyaml.safeLoad(req.body,['DEFAULT_SAFE_SCHEMA']);
+    //console.log(req.body.yaml);
+    var JString = jsyaml.safeLoad(req.body.yaml,['DEFAULT_SAFE_SCHEMA']);
     //var JString = jsyaml.safeLoad(doc,['DEFAULT_SAFE_SCHEMA']);
     //console.log('the string: ',JString);
     res.send(200,JString);
